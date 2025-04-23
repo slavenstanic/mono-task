5f51158a1a50a6652113353ce77db2cf607945ca
slaven <slaven.stanic@based-technology.hr>
2025-04-23 15:44:51 +0200

    changed VehicleEngineType to lookup table (only read method)

--- VehicleAppBackend/Project.Repository.Common/IVehicleEngineTypeRepository.cs
+++ VehicleAppBackend/Project.Repository.Common/IVehicleEngineTypeRepository.cs
@@ -4,8 +4,5 @@ namespace Project.Repository.Common;
 
 public interface IVehicleEngineTypeRepository
 {
-    Task<int> InsertAsync(IVehicleEngineType entity);
     Task<List<IVehicleEngineType>> GetAllAsync();
-    Task<int> UpdateAsync(IVehicleEngineType entity);
-    Task<int> DeleteAsync(int id);
 }
--- VehicleAppBackend/Project.Repository/VehicleEngineTypeRepository.cs
+++ VehicleAppBackend/Project.Repository/VehicleEngineTypeRepository.cs
@@ -15,40 +15,8 @@ public class VehicleEngineTypeRepository : IVehicleEngineTypeRepository
         _context = context;
     }
     
-    public async Task<int> InsertAsync(IVehicleEngineType entity)
-    {
-        var model = new VehicleEngineType
-        {
-            Type = entity.Type,
-            Abrv = entity.Abrv
-        };
-
-        _context.VehicleEngineTypes.Add(model);
-        return await _context.SaveChangesAsync();
-    }
-    
     public async Task<List<IVehicleEngineType>> GetAllAsync()
     {
         return await _context.VehicleEngineTypes.ToListAsync<IVehicleEngineType>();
-    }
-    
-    public async Task<int> UpdateAsync(IVehicleEngineType entity)
-    {
-        var existing = await _context.VehicleEngineTypes.FindAsync(entity.Id);
-        if (existing == null) return 0;
-        
-        existing.Type = entity.Type;
-        existing.Abrv = entity.Abrv;
-
-        return await _context.SaveChangesAsync();
-    }
-    
-    public async Task<int> DeleteAsync(int id)
-    {
-        var entity = await _context.VehicleEngineTypes.FindAsync(id);
-        if (entity == null) return 0;
-
-        _context.VehicleEngineTypes.Remove(entity);
-        return await _context.SaveChangesAsync();
     }
 }
