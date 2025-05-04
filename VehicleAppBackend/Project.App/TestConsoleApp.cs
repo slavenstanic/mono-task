2465e9a3fba354456e2076f9270a74e173bb5b0d
slaven <slaven.stanic@based-technology.hr>
2025-05-04 13:42:45 +0200

    added Ninject

---
+++ VehicleAppBackend/Project.App/TestConsoleApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Ninject;
+using Project.Repository.Common;
+using System.Threading.Tasks;
+
+namespace Project.App
+{
+    class TestConsoleApp
+    {
+        static async Task Main(string[] args)
+        {
+            var kernel = new StandardKernel(new NinjectBindings());
+
+            var unitOfWork = kernel.Get<IUnitOfWork>();
+
+            Console.WriteLine("VehicleMake unosi u bazi:");
+            var makes = await unitOfWork.VehicleMakes.GetAllAsync();
+
+            foreach (var make in makes)
+            {
+                Console.WriteLine($"{make.Id} - {make.Name} ({make.Abrv})");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
